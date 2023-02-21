dcdb026d417998b03048d73de47e1d8cf30d7cae
devyad24 <devyadav4211@gmail.com>
2023-02-22 00:28:18 +0530

    Create states to generate random characters when card is clicked.

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -8,19 +8,30 @@ import FireKeeper from './images/firekeeper.jpg'
 import Siegmeyer from './images/siegmeyer_of_catarina.jpg' 
 import Sif from './images/sif.jpg' 
 import Andre from './images/Andre.webp' 
+import { useState } from 'react'
 
 function App() {
-  const soulsCharacters = [{imgsrc: Solaire, imgName: "Solaire"}, {imgsrc:Tarkus, imgName: "Tarkus"}, {imgsrc: Crestfallen, imgName: "Crestfallen"}
+  const soulsCharacters = [{imgsrc: Solaire, imgName: "Solaire"}, {imgsrc: Tarkus, imgName: "Tarkus"}, {imgsrc: Crestfallen, imgName: "Crestfallen"}
                             , {imgsrc:Havel, imgName: "Havel"}, {imgsrc: FireKeeper, imgName: "Firekeeper"},
                             {imgsrc:Siegmeyer, imgName:"Siegmeyer"}, {imgsrc: Sif, imgName: "Sif"}, {imgsrc: Andre, imgName: "Andre"}]
   
+  // function generateRandomCharacter(){
+  //   const random = Math.ceil(Math.random() * 7)
+  //   return soulsCharacters[random]
+  // }
+  const [character, setRandomCharacter] = useState(soulsCharacters[0]);
+
+  function generateRandomCharacter(){
+    const random = Math.ceil(Math.random() * 7)
+    setRandomCharacter(soulsCharacters[random])
+    return soulsCharacters[random]
+  }
   function generateCards(){
-    let id = 1;
-    return soulsCharacters.map(character => {
-      return <Card className="card" cardID={id++} cardURL={character.imgsrc} 
-                cardName={character.imgName} />
+    return soulsCharacters.map((character, index) => {
+      return <Card key={index} className="card" cardID={index} cardURL={character.imgsrc} cardName={character.imgName} randomChar={generateRandomCharacter}/>
     })
   }
+
   return (
     <div className="App">
       <Navbar />
--- memory-card/src/Components/Card.js
+++ memory-card/src/Components/Card.js
@@ -4,10 +4,25 @@ export default function Card(props){
     const [card, setCard] = useState({
         id: props.cardID,
         name: props.cardName,
-        url:props.cardURL
+        url:props.cardURL,
+        clicked: false
     }) 
+
+    function handleClick(){
+        const newChar = props.randomChar()
+        setCard(prevCard => {
+            //take the random character from the props and then modify the cards state to the new values
+            return {
+                ...prevCard,
+                name: newChar.imgName,
+                url: newChar.imgsrc,
+                clicked: true
+            }
+        })
+        console.log(`Card ${card.id} was clicked!`)
+    }
     return (
-        <div className="card">
+        <div className="card" onClick={handleClick}>
             <img src={card.url} />
             <p>{card.name}</p>
         </div>
